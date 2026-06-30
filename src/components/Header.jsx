54d1880be42c0b046b9084da49f946073fe55913
aico0315 <aiko.phoenix12@gmail.com>
2026-06-30 09:36:04 +0900

    feat:add Dark-themeToggle Animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({ onToggleTheme, className }){
   return (
     <header className={className}>
       <h1 className="web-title">STUDY</h1>
-      <button onClick={onToggleTheme} className="theme-toggle" aria-label="ダークモードに切り替え">
+      <button onClick={onToggleTheme} className={`theme-toggle ${className}`} aria-label="ダークモードに切り替え">
         <span className="knob"></span>
       </button>
     </header>
