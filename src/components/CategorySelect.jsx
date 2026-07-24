1d2a13c4b47dea7c59c894fd233c57b1a133bbe7
aico0315 <aiko.phoenix12@gmail.com>
2026-07-24 17:16:18 +0900

    feat: add scrolling to categorySelect

--- src/components/CategorySelect.jsx
+++ src/components/CategorySelect.jsx
@@ -4,17 +4,19 @@ import "./Dashboard.css";
 import "./CategorySelect.css";
 import getSortedCategories from "../utils/getSortedCategories";
 
-export default function CategorySelect ({ className, wordArray, onUpdate, onClose, onDisplay, onScreenLifecycle }){
+export default function CategorySelect ({ className, wordArray, onUpdate, onClose, onScreenLifecycle }){
   const categories = getSortedCategories(wordArray);
 
   return(
     <div className={`modal-overlay ${className}`}>
       <div className="mode-select-area menu-list active">
-        {categories.map(elem=>
-          <Button className="category-btns-container category-btns" variant="text" label={elem} key={elem} onUpdate={onUpdate} onPhaseChange={onDisplay} onPhaseChange={onScreenLifecycle}/>
-        )}
-        <Button className="weak-words-btn category-btns" variant="text" label="苦手な単語"/>
-        <Button className="question-continue-btn category-btns" variant="text" label="つづきから"/>
+        <div className="category-list-scroll-area">
+          {categories.map(elem=>
+            <Button className="category-btns-container category-btns" variant="text" label={elem} key={elem} onUpdate={onUpdate} onPhaseChange={onScreenLifecycle}/>
+          )}
+          <Button className="weak-words-btn category-btns" variant="text" label="苦手な単語"/>
+        </div>
+        {/* <Button className="question-continue-btn category-btns" variant="text" label="つづきから"/> */}
         <Button className="close-btn" variant="subtle" label="× 閉じる" onClick={onUpdate} onPhaseChange={onClose}/>
       </div>
     </div>
